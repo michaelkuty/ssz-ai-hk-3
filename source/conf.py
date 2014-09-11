0e8e3b7e51b858a092434ea29369fe34d5b1e950
Michael Kuty <6du1ro.n@gmail.com>
2014-09-11 10:03:11 +0200

    něco málo o grafice

--- source/conf.py
+++ source/conf.py
@@ -21,6 +21,8 @@ import sphinx_bootstrap_theme
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
+#html_theme = 'haiku'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
